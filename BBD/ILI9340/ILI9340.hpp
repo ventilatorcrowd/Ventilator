dd70fc3ec5c0e32c28d25f4f84c18cfa8d11d519
s.gilbert <simon.gilbert@animal-dynamics.com>
2020-04-17 02:09:48 +0100

    Moved colours to display base class.

--- BBD/ILI9340/ILI9340.hpp
+++ BBD/ILI9340/ILI9340.hpp
@@ -31,26 +31,6 @@ DEFINITIONS
 #define TFT_MW                  (TFT_WIDTH - 1)
 #define TFT_MH                  (TFT_HEIGHT - 1)
 
-#define BLACK       0x0000
-#define NAVY        0x000F
-#define DARKGREEN   0x03E0
-#define DARKCYAN    0x03EF
-#define MAROON      0x7800
-#define PURPLE      0x780F
-#define OLIVE       0x7BE0
-#define LIGHTGREY   0xC618
-#define DARKGREY    0x7BEF
-#define BLUE        0x001F
-#define GREEN       0x07E0
-#define CYAN        0x07FF
-#define RED         0xF800
-#define MAGENTA     0xF81F
-#define YELLOW      0xFFE0
-#define WHITE       0xFFFF
-#define ORANGE      0xFD20
-#define GREENYELLOW 0xAFE5
-#define PINK        0xF81F
-
 #define _CMD_NOP                0x00//No Operation
 #define _CMD_SWRESET            0x01//Software Reset
 #define _CMD_SLPIN              0x10//Enter Sleep Mode
